e8a9ebe248f0cb4d093bd87ca5dd1f4c90fd497a
fengc_cn1 <49382219+fengccn@users.noreply.github.com>
2026-07-18 01:23:14 +0800

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'react-toastify/ReactToastify.min.css';
 import 'react-photo-view/dist/react-photo-view.css';
 import { GoogleAnalytics } from '@next/third-parties/google'
-
+import { SessionProvider } from "next-auth/react";  // ← 新增这行
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,12 +13,14 @@ export const metadata = {
   description: "图床",
 };
 
-
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <SessionProvider>  {/* ← 新增这层包裹 */}
+          {children}
+        </SessionProvider>
+      </body>
       <GoogleAnalytics gaId="G-JVKEXR5XSG" />
     </html>
   );
